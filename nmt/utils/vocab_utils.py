2ec8ee9895af1d11f3e8c77b5363b61e7e63802a
lcy-seso <lcy.seso@gmail.com>
2018-04-27 14:33:00 +0800

    format the codes and add pre-commit config.

--- nmt/utils/vocab_utils.py
+++ nmt/utils/vocab_utils.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """Utility to handle vocabularies."""
 
 from __future__ import absolute_import
@@ -27,7 +26,6 @@ from tensorflow.python.ops import lookup_ops
 
 from ..utils import misc_utils as utils
 
-
 UNK = "<unk>"
 SOS = "<s>"
 EOS = "</s>"
@@ -35,61 +33,66 @@ UNK_ID = 0
 
 
 def load_vocab(vocab_file):
-  vocab = []
-  with codecs.getreader("utf-8")(tf.gfile.GFile(vocab_file, "rb")) as f:
-    vocab_size = 0
-    for word in f:
-      vocab_size += 1
-      vocab.append(word.strip())
-  return vocab, vocab_size
+    vocab = []
+    with codecs.getreader("utf-8")(tf.gfile.GFile(vocab_file, "rb")) as f:
+        vocab_size = 0
+        for word in f:
+            vocab_size += 1
+            vocab.append(word.strip())
+    return vocab, vocab_size
 
 
-def check_vocab(vocab_file, out_dir, check_special_token=True, sos=None,
-                eos=None, unk=None):
-  """Check if vocab_file doesn't exist, create from corpus_file."""
-  if tf.gfile.Exists(vocab_file):
-    utils.print_out("# Vocab file %s exists" % vocab_file)
-    vocab, vocab_size = load_vocab(vocab_file)
-    if check_special_token:
-      # Verify if the vocab starts with unk, sos, eos
-      # If not, prepend those tokens & generate a new vocab file
-      if not unk: unk = UNK
-      if not sos: sos = SOS
-      if not eos: eos = EOS
-      assert len(vocab) >= 3
-      if vocab[0] != unk or vocab[1] != sos or vocab[2] != eos:
-        utils.print_out("The first 3 vocab words [%s, %s, %s]"
-                        " are not [%s, %s, %s]" %
-                        (vocab[0], vocab[1], vocab[2], unk, sos, eos))
-        vocab = [unk, sos, eos] + vocab
-        vocab_size += 3
-        new_vocab_file = os.path.join(out_dir, os.path.basename(vocab_file))
-        with codecs.getwriter("utf-8")(
-            tf.gfile.GFile(new_vocab_file, "wb")) as f:
-          for word in vocab:
-            f.write("%s\n" % word)
-        vocab_file = new_vocab_file
-  else:
-    raise ValueError("vocab_file '%s' does not exist." % vocab_file)
+def check_vocab(vocab_file,
+                out_dir,
+                check_special_token=True,
+                sos=None,
+                eos=None,
+                unk=None):
+    """Check if vocab_file doesn't exist, create from corpus_file."""
+    if tf.gfile.Exists(vocab_file):
+        utils.print_out("# Vocab file %s exists" % vocab_file)
+        vocab, vocab_size = load_vocab(vocab_file)
+        if check_special_token:
+            # Verify if the vocab starts with unk, sos, eos
+            # If not, prepend those tokens & generate a new vocab file
+            if not unk: unk = UNK
+            if not sos: sos = SOS
+            if not eos: eos = EOS
+            assert len(vocab) >= 3
+            if vocab[0] != unk or vocab[1] != sos or vocab[2] != eos:
+                utils.print_out("The first 3 vocab words [%s, %s, %s]"
+                                " are not [%s, %s, %s]" %
+                                (vocab[0], vocab[1], vocab[2], unk, sos, eos))
+                vocab = [unk, sos, eos] + vocab
+                vocab_size += 3
+                new_vocab_file = os.path.join(out_dir,
+                                              os.path.basename(vocab_file))
+                with codecs.getwriter("utf-8")(tf.gfile.GFile(
+                        new_vocab_file, "wb")) as f:
+                    for word in vocab:
+                        f.write("%s\n" % word)
+                vocab_file = new_vocab_file
+    else:
+        raise ValueError("vocab_file '%s' does not exist." % vocab_file)
 
-  vocab_size = len(vocab)
-  return vocab_size, vocab_file
+    vocab_size = len(vocab)
+    return vocab_size, vocab_file
 
 
 def create_vocab_tables(src_vocab_file, tgt_vocab_file, share_vocab):
-  """Creates vocab tables for src_vocab_file and tgt_vocab_file."""
-  src_vocab_table = lookup_ops.index_table_from_file(
-      src_vocab_file, default_value=UNK_ID)
-  if share_vocab:
-    tgt_vocab_table = src_vocab_table
-  else:
-    tgt_vocab_table = lookup_ops.index_table_from_file(
-        tgt_vocab_file, default_value=UNK_ID)
-  return src_vocab_table, tgt_vocab_table
+    """Creates vocab tables for src_vocab_file and tgt_vocab_file."""
+    src_vocab_table = lookup_ops.index_table_from_file(
+        src_vocab_file, default_value=UNK_ID)
+    if share_vocab:
+        tgt_vocab_table = src_vocab_table
+    else:
+        tgt_vocab_table = lookup_ops.index_table_from_file(
+            tgt_vocab_file, default_value=UNK_ID)
+    return src_vocab_table, tgt_vocab_table
 
 
 def load_embed_txt(embed_file):
-  """Load embed_file into a python dictionary.
+    """Load embed_file into a python dictionary.
 
   Note: the embed_file should be a Glove formated txt file. Assuming
   embed_size=5, for example:
@@ -103,16 +106,17 @@ def load_embed_txt(embed_file):
   Returns:
     a dictionary that maps word to vector, and the size of embedding dimensions.
   """
-  emb_dict = dict()
-  emb_size = None
-  with codecs.getreader("utf-8")(tf.gfile.GFile(embed_file, 'rb')) as f:
-    for line in f:
-      tokens = line.strip().split(" ")
-      word = tokens[0]
-      vec = list(map(float, tokens[1:]))
-      emb_dict[word] = vec
-      if emb_size:
-        assert emb_size == len(vec), "All embedding size should be same."
-      else:
-        emb_size = len(vec)
-  return emb_dict, emb_size
+    emb_dict = dict()
+    emb_size = None
+    with codecs.getreader("utf-8")(tf.gfile.GFile(embed_file, 'rb')) as f:
+        for line in f:
+            tokens = line.strip().split(" ")
+            word = tokens[0]
+            vec = list(map(float, tokens[1:]))
+            emb_dict[word] = vec
+            if emb_size:
+                assert emb_size == len(
+                    vec), "All embedding size should be same."
+            else:
+                emb_size = len(vec)
+    return emb_dict, emb_size
